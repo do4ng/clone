acec1db08ddd5a89afb295113e8abf561848b872
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2021-07-05 13:40:27 +0900

    Create app.d.ts

---
+++ app.d.ts
@@ -0,0 +1,21 @@
+/**
+ * Clone directory
+ * @param dir Input Directory
+ * @param out Output Directory
+ * @param output Allow Output (console)
+ */
+export function clone(dir: string, out: string, output: boolean): void
+
+/**
+ * Check Directory
+ * @param dir Directory
+ */
+export function isDirectory(dir: string): boolean
+
+/**
+ * Clone directory without Create Folder
+ * @param dir 
+ * @param out 
+ * @param output 
+ */
+export function read(dir: string, out: string, output: boolean): void
